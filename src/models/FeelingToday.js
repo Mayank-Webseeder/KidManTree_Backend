7e6a6a5fb32e816f63ca050ba3b0741fcf8ce919
govindweb777 <prajapatigovind866@gmail.com>
2025-10-08 12:44:15 +0530

    fix feeling api

--- src/models/FeelingToday.js
+++ src/models/FeelingToday.js
@@ -1,32 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const feelingTodaySchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+const feelingTodaySchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    feeling: {
+      type: String,
+      enum: [
+        "Crying",
+        "Sad",
+        "Exhausted",
+        "Calm",
+        "Happy",
+        "Cheerful",
+        "Energetic",
+        "Confused",
+        "Anxious",
+      ],
+      required: true,
+    },
+    emoji: {
+      type: String,
+      required: true,
+    },
+    notes: {
+      type: String,
+      maxlength: 500,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  feeling: {
-    type: String,
-    enum: ['excellent', 'good', 'okay', 'not-great', 'unsatisfied'],
-    required: true
-  },
-  emoji: {
-    type: String,
-    required: true
-  },
-  notes: {
-    type: String,
-    maxlength: 500
-  },
-  date: {
-    type: Date,
-    default: Date.now
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
 feelingTodaySchema.index({ user: 1, date: -1 });
 
-module.exports = mongoose.model('FeelingToday', feelingTodaySchema);
+module.exports = mongoose.model("FeelingToday", feelingTodaySchema);
